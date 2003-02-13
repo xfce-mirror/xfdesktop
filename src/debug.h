de997c32d7963f6f0109f55d2ccb3016f289a3a2
Biju Chacko <bchacko@redhat.com>
2003-02-13 04:10:18 +0000

    Made 'debug.h' more portable, mostly to fix compile errors on BSD
    (Old svn revision: 7200)

--- src/debug.h
+++ src/debug.h
@@ -17,37 +17,25 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
-/* Version 2.4 and later of GCC define a magical variable `__PRETTY_FUNCTION__'
-   which contains the name of the function currently being defined.
-   This is broken in G++ before version 2.6.
-   C9x has a similar variable called __func__, but prefer the GCC one since
-   it demangles C++ function names.  
-# if defined __cplusplus ? __GNUC_PREREQ (2, 6) : __GNUC_PREREQ (2, 4)
-#   define __DBG_FUNCTION	__PRETTY_FUNCTION__
+# if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
+#  define __DBG_FUNCTION	__func__
 # else
-#  if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
-#   define __DBG_FUNCTION	__func__
-#  else
-#   define __DBG_FUNCTION	((__const char *) 0)
-#  endif
+#  define __DBG_FUNCTION	((__const char *) 0)
 # endif
-*/
+
 #if defined(DEBUG) && DEBUG
 #include <stdio.h>
 
-/* this will break for gcc < 2.6; does anyone really that anymore? 
- * May also break non-gcc compilers <-- don't compile with debug then! */
-#define __DBG_FUNCTION	__PRETTY_FUNCTION__
 
-/*#define DBG(fmt, args...) ({fprintf(stderr, __FILE__ ", line %d: ", __LINE__); fprintf(stderr , fmt , ## args );})*/
-#define DBG(fmt, args...) 																\
+# define DBG(fmt, args...) 																\
 		({																				\
 			fprintf(stderr, "\n%s: line %d: %s(): ", __FILE__ , __LINE__, __DBG_FUNCTION); 	\
 			fprintf(stderr , fmt , ## args );											\
 		})
 #else
-/* #define DBG(fmt, args...) do {} while(0) */
-#define DBG(fmt, args...)
+
+# define DBG(fmt, args...)
+
 #endif
 
 #if defined(DEBUG_TRACE) && DEBUG_TRACE
