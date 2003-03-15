03841b8f5f10896b1c8ffbb575c6a0ac8759f176
Benedikt Meurer <benny@xfce.org>
2003-03-15 09:24:05 +0000

    Removed --enable-debug=full
    (Old svn revision: 7227)

--- autogen.sh
+++ autogen.sh
@@ -142,7 +142,7 @@ do
   fi
 done
 
-conf_flags="--enable-debug=full --enable-maintainer-mode"
+conf_flags="--enable-maintainer-mode"
 
 if test x$NOCONFIGURE = x; then
   echo Running $srcdir/configure $conf_flags "$@" ...
