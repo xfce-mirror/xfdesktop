0ac1a7bc79e79fea02e407f9e70291c3583e9adc
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2004-02-06 17:58:50 +0000

    Revert to #336699
    (Old svn revision: 7489)

--- settings/backdrop_settings.c
+++ settings/backdrop_settings.c
@@ -223,9 +223,9 @@ backdrop_create_channel (McsPlugin * mcs_plugin)
 	/* 
 	   Just a color by default #336699 - That number looks cool :)
 	 */
-	backdrop_color.red = (guint16) 0x9000;
-	backdrop_color.green = (guint16) 0xA000;
-	backdrop_color.blue = (guint16) 0xB000;
+	backdrop_color.red = (guint16) 0x3300;
+	backdrop_color.green = (guint16) 0x6600;
+	backdrop_color.blue = (guint16) 0x9900;
 	backdrop_color.alpha = (guint16) 0;
 	mcs_manager_set_color (mcs_plugin->manager, "color", BACKDROP_CHANNEL,
 			       &backdrop_color);
