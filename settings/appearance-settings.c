f84c6d8b6970371485ba5353e828057a160035a5
Brian Tarricone <brian@tarricone.org>
2006-02-24 21:48:03 +0000

    set _NET_WM_USER_TIME before showing settings dialog
    (Old svn revision: 20036)

--- settings/appearance-settings.c
+++ settings/appearance-settings.c
@@ -1253,6 +1253,7 @@ run_dialog (McsPlugin * mcs_plugin)
     g_signal_connect(G_OBJECT(bd->dialog), "response",
             G_CALLBACK(run_dialog_cb), bd);
     gtk_window_set_modal(GTK_WINDOW(bd->dialog), FALSE);
+    gdk_x11_window_set_user_time(bd->dialog->window, GDK_CURRENT_TIME);
     gtk_widget_show(bd->dialog);
 }
 
