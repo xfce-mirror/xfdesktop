74763b862f6419c958ef1bfedaaa4a73f86f25fe
Brian J. Tarricone <brian@tarricone.org>
2009-08-18 21:28:19 -0700

    require x-d-t 4.7.0; remove revision and lang substitutions

--- autogen.sh
+++ autogen.sh
@@ -2,44 +2,26 @@
 #
 # $Id$
 #
-# Copyright (c) 2002-2005
+# Copyright (c) 2002-2009
 #         The Xfce development team. All rights reserved.
 #
-# Written for Xfce by Benedikt Meurer <benny@xfce.org>.
+# Written for Xfce by Benedikt Meurer <benny@xfce.org>
+#                 and Brian Tarricone <brian@tarricone.org>.
 #
 
-(type xdt-autogen) >/dev/null 2>&1 || {
+export XDT_REQUIRED_VERSION="4.7.0"
+
+(type xdt-autogen && xdt-autogen --version) >/dev/null 2>&1 || {
   cat >&2 <<EOF
-autogen.sh: You don't seem to have the Xfce development tools installed on
-            your system, which are required to build this software.
-            Please install the xfce4-dev-tools package first, it is available
+autogen.sh: You don't seem to have the Xfce development tools (at least
+            version $XDT_REQURED_VERSION) installed on your system, which
+            are required to build this software.
+            Please install the xfce4-dev-tools package first; it is available
             from http://www.xfce.org/.
 EOF
   exit 1
 }
 
-# verify that po/LINGUAS is present
-(test -f po/LINGUAS) >/dev/null 2>&1 || {
-  cat >&2 <<EOF
-autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
-            or try to checkout again.
-EOF
-  exit 1
-}
-
-# substitute revision and linguas
-linguas=`sed -e '/^#/d' po/LINGUAS`
-if [ -d .git ]; then
-  revision=$(git rev-parse --short HEAD)
-fi
-if [ -z "$revision" ]; then
-  revision=UNKNOWN
-fi
-
-sed -e "s/@LINGUAS@/${linguas}/g" \
-    -e "s/@REVISION@/${revision}/g" \
-    < "configure.ac.in" > "configure.ac"
-
-exec xdt-autogen $@
+exec xdt-autogen "$@"
 
 # vi:set ts=2 sw=2 et ai:
