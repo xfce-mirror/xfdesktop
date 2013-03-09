xfce-mirror/xfdesktop

commit ebad377e5cd067cec9f2b402dff4991ddc4cc3b5

SVG images are no longer pixilated when scaled up
The code was rendering the svg image to a pixbuf and then scaling
it rather than requesting the image at the desired size when creating
the pixbuf.

src/xfce-backdrop.c
@@ -933,17 +933,10 @@ xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
     
     g_return_val_if_fail(XFCE_IS_BACKDROP(backdrop), NULL);
     
-    if(backdrop->priv->show_image && backdrop->priv->image_path) {
-        image = gdk_pixbuf_new_from_file(backdrop->priv->image_path, NULL);
-        if(image) {
-            iw = gdk_pixbuf_get_width(image);
-            ih = gdk_pixbuf_get_height(image);
-        }
-    }
-    
+    if(backdrop->priv->show_image && backdrop->priv->image_path)
+        gdk_pixbuf_get_file_info(backdrop->priv->image_path, &iw, &ih);
+
     if(backdrop->priv->width == 0 || backdrop->priv->height == 0) {
-        if(!image)
-            return NULL;
         w = iw;
         h = ih;
     } else {
@@ -963,7 +956,9 @@ xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
             final_image = create_solid(&backdrop->priv->color1, w, h, FALSE, 0xff);
     }
     
-    if(!image) {
+    /*check if the file exists,
+     *and if it doesn't then make the background the single colour*/
+    if(!g_file_test(backdrop->priv->image_path, G_FILE_TEST_EXISTS)) {
         if(backdrop->priv->brightness != 0)
             final_image = adjust_brightness(final_image, backdrop->priv->brightness);
         
@@ -1001,6 +996,7 @@ xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
     
     switch(istyle) {
         case XFCE_BACKDROP_IMAGE_CENTERED:
+            image = gdk_pixbuf_new_from_file(backdrop->priv->image_path, NULL);
             dx = MAX((w - iw) / 2, 0);
             dy = MAX((h - ih) / 2, 0);
             xo = MIN((w - iw) / 2, dx);
@@ -1011,6 +1007,7 @@ xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
             break;
         
         case XFCE_BACKDROP_IMAGE_TILED:
+            image = gdk_pixbuf_new_from_file(backdrop->priv->image_path, NULL);
             tmp = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, w, h);
             for(i = 0; (i * iw) < w; i++) {
                 for(j = 0; (j * ih) < h; j++) {
@@ -1033,10 +1030,10 @@ xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
             break;
         
         case XFCE_BACKDROP_IMAGE_STRETCHED:
-            xscale = (gdouble)w / iw;
-            yscale = (gdouble)h / ih;
+            image = gdk_pixbuf_new_from_file_at_scale(
+                            backdrop->priv->image_path, w, h, FALSE, NULL);
             gdk_pixbuf_composite(image, final_image, 0, 0, w, h,
-                    0, 0, xscale, yscale, interp, 255);
+                    0, 0, 1, 1, interp, 255);
             break;
         
         case XFCE_BACKDROP_IMAGE_SCALED:
@@ -1053,9 +1050,12 @@ xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
             }
             dx = xo;
             dy = yo;
-            
+
+            image = gdk_pixbuf_new_from_file_at_scale(
+                        backdrop->priv->image_path, iw * xscale,
+                        ih * yscale, TRUE, NULL);
             gdk_pixbuf_composite(image, final_image, dx, dy,
-                    iw * xscale, ih * yscale, xo, yo, xscale, yscale,
+                    iw * xscale, ih * yscale, xo, yo, 1, 1,
                     interp, 255);
             break;
         
@@ -1071,8 +1071,12 @@ xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
                 xo = 0;
                 yo = (h - (ih * yscale)) * 0.5;
             }
+
+            image = gdk_pixbuf_new_from_file_at_scale(
+                                backdrop->priv->image_path, iw * xscale,
+                                ih * yscale, TRUE, NULL);
             gdk_pixbuf_composite(image, final_image, 0, 0,
-                    w, h, xo, yo, xscale, yscale, interp, 255);
+                    w, h, xo, yo, 1, 1, interp, 255);
             break;
         
         default:

commit a22643103bf113f58fb31c90c5a8963457d0783c

Fix hang when no backdrop image is selected (Bug #9892)
Checking whether backdrop->priv->image_path exists isn't enough; we'd
also have to check whether backdrop->priv->show_image is TRUE before
proceeding to apply a backdrop image.

When backdrop->priv->show_image is FALSE, iw and ih (image width/height)
will be zero, but can later be used to compute the upper limit of a for
loop as well as other computations.

To solve this, a new boolean variable (apply_backdrop_image) is added
and set to TRUE only if the return value of gdk_pixbuf_get_file_info()
is not NULL (meaning that the image format is recognized).

The above logic error was introduced in commit
ebad377e5cd067cec9f2b402dff4991ddc4cc3b5.

src/xfce-backdrop.c
@@ -924,6 +924,8 @@ GdkPixbuf *
 xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
 {
     GdkPixbuf *final_image, *image = NULL, *tmp;
+    GdkPixbufFormat *format = NULL;
+    gboolean apply_backdrop_image = FALSE;
     gint i, j;
     gint w, h, iw = 0, ih = 0;
     XfceBackdropImageStyle istyle;
@@ -933,8 +935,12 @@ xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
     
     g_return_val_if_fail(XFCE_IS_BACKDROP(backdrop), NULL);
     
-    if(backdrop->priv->show_image && backdrop->priv->image_path)
-        gdk_pixbuf_get_file_info(backdrop->priv->image_path, &iw, &ih);
+    if(backdrop->priv->show_image && backdrop->priv->image_path) {
+        format = gdk_pixbuf_get_file_info(backdrop->priv->image_path, &iw, &ih);
+        /* make sure we have a usable backdrop image */
+        if(format != NULL)
+            apply_backdrop_image = TRUE;
+    }
 
     if(backdrop->priv->width == 0 || backdrop->priv->height == 0) {
         w = iw;
@@ -956,9 +962,7 @@ xfce_backdrop_get_pixbuf(XfceBackdrop *backdrop)
             final_image = create_solid(&backdrop->priv->color1, w, h, FALSE, 0xff);
     }
     
-    /*check if the file exists,
-     *and if it doesn't then make the background the single colour*/
-    if(!g_file_test(backdrop->priv->image_path, G_FILE_TEST_EXISTS)) {
+    if(!apply_backdrop_image) {
         if(backdrop->priv->brightness != 0)
             final_image = adjust_brightness(final_image, backdrop->priv->brightness);
         