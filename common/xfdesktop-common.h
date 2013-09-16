41929e74d8ea59366e0368fcd0ca7a90b70f918d
Eric Koegel <eric.koegel@gmail.com>
2013-09-16 10:17:02 +0300

    Fix FSF Address

--- common/xfdesktop-common.h
+++ common/xfdesktop-common.h
@@ -15,8 +15,8 @@
  *  GNU Library General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  *  Random portions taken from or inspired by the original xfdesktop for xfce4:
  *     Copyright (C) 2002-2003 Jasper Huijsmans (huysmans@users.sourceforge.net)
--- common/xfdesktop-thumbnailer.h
+++ common/xfdesktop-thumbnailer.h
@@ -16,8 +16,8 @@
  *  GNU Library General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  *  xfdesktop-thumbnailer is based on thumbnailer code from Ristretto
  *  Copyright (c) Stephan Arts 2009-2011 <stephan@xfce.org>
