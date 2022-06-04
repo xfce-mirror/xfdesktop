d7c56e1223513ed11aafc224e0fea5fee06bcc80
Alexander Schwinn <alexxcons@xfce.org>
2022-06-04 23:33:16 +0000

    style fix

--- src/xfdesktop-file-utils.c
+++ src/xfdesktop-file-utils.c
@@ -1244,7 +1244,7 @@ xfdesktop_file_utils_execute(GFile *working_directory,
     if(fileman_proxy) {
         GError *error = NULL;
         gchar *working_dir = working_directory != NULL ? g_file_get_uri(working_directory) : NULL;
-        const gchar * path_prop;
+        const gchar *path_prop;
         gchar *uri = g_file_get_uri(file);
         gchar *display_name = g_strdup(gdk_display_get_name(gdk_screen_get_display(screen)));
         gchar *startup_id = g_strdup_printf("_TIME%d", gtk_get_current_event_time());
