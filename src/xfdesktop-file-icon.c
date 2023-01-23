85eedb73764f3b4c939607a20174cc317be1f893
Brian J. Tarricone <brian@tarricone.org>
2023-01-23 02:21:55 -0800

    Allow opening multiple selected folder icons from context menu
    Previously we would just get a g_warning about more then one icon being
    selected.
    
    Closes #229.

--- src/xfdesktop-file-icon.c
+++ src/xfdesktop-file-icon.c
@@ -150,12 +150,18 @@ xfdesktop_file_icon_activated(XfdesktopIcon *icon)
     toplevel = gtk_widget_get_toplevel(icon_view);
     gscreen = gtk_widget_get_screen(icon_view);
 
-    if(g_file_info_get_file_type(info) == G_FILE_TYPE_DIRECTORY)
-        xfdesktop_file_utils_open_folder(file, gscreen, GTK_WINDOW(toplevel));
-    else if(xfdesktop_file_utils_file_is_executable(info))
+    if(g_file_info_get_file_type(info) == G_FILE_TYPE_DIRECTORY) {
+        GList link = {
+            .data = file,
+            .prev = NULL,
+            .next = NULL,
+        };
+        xfdesktop_file_utils_open_folders(&link, gscreen, GTK_WINDOW(toplevel));
+    } else if(xfdesktop_file_utils_file_is_executable(info)) {
         xfdesktop_file_utils_execute(NULL, file, NULL, gscreen, GTK_WINDOW(toplevel));
-    else
+    } else {
         xfdesktop_file_utils_launch(file, gscreen, GTK_WINDOW(toplevel));
+    }
 
     return TRUE;
 }
