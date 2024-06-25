434325b1941cbd163da27ef1e69b4a3cb1ab5e05
Brian J. Tarricone <brian@tarricone.org>
2024-06-25 13:24:02 -0700

    Rewrite window icon manager to display on all monitors
    I've moved window enumeration and watching to the model, which honestly
    is where it's always belonged.  To make things simpler, the model always
    includes all windows, regardless of their state.  Each icon view has a
    GtkTreeModelFilter wrapped around the model, which filters out windows
    that should not be shown on the current monitor/workspace (or at all,
    because they're not minimized, or have the skip-taskbar hint set).
    
    This includes the minimal amount of changes necessary to also keep the
    file icon manager compiling and more or less working (albeit only on the
    primary monitor).
    
    As much as I usually dislike rewrites, this turned out to be a lot
    easier than refactoring the existing code, which was not amenable to
    this new way of looking at things.

--- src/xfdesktop-icon-view-holder.h
+++ src/xfdesktop-icon-view-holder.h
@@ -1,7 +1,7 @@
 /*
  *  xfdesktop - xfce4's desktop manager
  *
- *  Copyright (c) 2006 Brian Tarricone, <brian@tarricone.org>
+ *  Copyright (c) 2024 Brian Tarricone, <brian@tarricone.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,24 +18,26 @@
  *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
-#ifndef __XFDESKTOP_WINDOW_ICON_MANAGER_H__
-#define __XFDESKTOP_WINDOW_ICON_MANAGER_H__
+#ifndef __XFDESKTOP_ICON_VIEW_HOLDER_H__
+#define __XFDESKTOP_ICON_VIEW_HOLDER_H__
 
-#include <glib-object.h>
-#include <xfconf/xfconf.h>
 #include <libxfce4windowing/libxfce4windowing.h>
 
-#include "xfdesktop-icon-view-manager.h"
+#include "xfce-desktop.h"
+#include "xfdesktop-icon-view.h"
 
 G_BEGIN_DECLS
 
-G_DECLARE_FINAL_TYPE(XfdesktopWindowIconManager, xfdesktop_window_icon_manager, XFDESKTOP, WINDOW_ICON_MANAGER, XfdesktopIconViewManager)
-#define XFDESKTOP_TYPE_WINDOW_ICON_MANAGER (xfdesktop_window_icon_manager_get_type())
+G_DECLARE_FINAL_TYPE(XfdesktopIconViewHolder, xfdesktop_icon_view_holder, XFDESKTOP, ICON_VIEW_HOLDER, GObject)
+#define XFDESKTOP_TYPE_ICON_VIEW_HOLDER (xfdesktop_icon_view_holder_get_type())
 
-XfdesktopIconViewManager *xfdesktop_window_icon_manager_new(XfwScreen *screen,
-                                                            XfconfChannel *channel,
-                                                            GList *desktops);
+XfdesktopIconViewHolder *xfdesktop_icon_view_holder_new(XfwScreen *screen,
+                                                        XfceDesktop *desktop,
+                                                        XfdesktopIconView *icon_view);
+
+XfceDesktop *xfdesktop_icon_view_holder_get_desktop(XfdesktopIconViewHolder *holder);
+XfdesktopIconView *xfdesktop_icon_view_holder_get_icon_view(XfdesktopIconViewHolder *holder);
 
 G_END_DECLS
 
-#endif  /* __XFDESKTOP_WINDOW_ICON_MANAGER_H__ */
+#endif  /* __XFDESKTOP_ICON_VIEW_HOLDER_H__ */
