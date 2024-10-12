64963337c216c2d3e5dea1a5eed47b83d1fa1ee8
Brian J. Tarricone <brian@tarricone.org>
2024-10-11 22:15:38 -0700

    Update icon view's item_under_pointer on button presses as well
    Closes #334

--- src/xfdesktop-icon-view.c
+++ src/xfdesktop-icon-view.c
@@ -1483,6 +1483,33 @@ xfdesktop_icon_view_set_cursor(XfdesktopIconView *icon_view, ViewItem *item, gbo
     icon_view->draw_focus = from_keyboard;
 }
 
+static void
+update_item_under_pointer(XfdesktopIconView *icon_view, GdkWindow *event_window, gdouble x, gdouble y) {
+    ViewItem *old_item_under_pointer = icon_view->item_under_pointer;
+    icon_view->item_under_pointer = xfdesktop_icon_view_widget_coords_to_item_internal(icon_view, x, y);
+
+    if (old_item_under_pointer != icon_view->item_under_pointer) {
+        if (old_item_under_pointer != NULL) {
+            xfdesktop_icon_view_invalidate_item(icon_view, old_item_under_pointer, FALSE);
+        }
+
+        if (icon_view->item_under_pointer != NULL) {
+            if (icon_view->single_click) {
+                GdkDisplay *display = gtk_widget_get_display(GTK_WIDGET(icon_view));
+                GdkCursor *cursor = gdk_cursor_new_for_display(display, GDK_HAND2);
+                gdk_window_set_cursor(event_window, cursor);
+                g_object_unref(cursor);
+            }
+
+            xfdesktop_icon_view_invalidate_item(icon_view, icon_view->item_under_pointer, FALSE);
+        } else {
+            if (icon_view->single_click) {
+                gdk_window_set_cursor(event_window, NULL);
+            }
+        }
+    }
+}
+
 static gboolean
 xfdesktop_icon_view_button_press(GtkWidget *widget, GdkEventButton *evt) {
     XfdesktopIconView *icon_view = XFDESKTOP_ICON_VIEW(widget);
@@ -1492,6 +1519,8 @@ xfdesktop_icon_view_button_press(GtkWidget *widget, GdkEventButton *evt) {
     xfdesktop_icon_view_cancel_keyboard_navigation(icon_view);
 
     gtk_widget_grab_focus(widget);
+
+    update_item_under_pointer(icon_view, evt->window, evt->x, evt->y);
 
     if(evt->type == GDK_BUTTON_PRESS) {
         GList *item_l;
@@ -2106,28 +2135,7 @@ xfdesktop_icon_view_motion_notify(GtkWidget *widget, GdkEventMotion *evt) {
         }
     } else {
         /* normal movement; highlight icons as they go under the pointer */
-        ViewItem *old_item_under_pointer = icon_view->item_under_pointer;
-        icon_view->item_under_pointer = xfdesktop_icon_view_widget_coords_to_item_internal(icon_view, evt->x, evt->y);
-
-        if (old_item_under_pointer != icon_view->item_under_pointer) {
-            if (old_item_under_pointer != NULL) {
-                xfdesktop_icon_view_invalidate_item(icon_view, old_item_under_pointer, FALSE);
-            }
-
-            if (icon_view->item_under_pointer != NULL) {
-                if (icon_view->single_click) {
-                    GdkCursor *cursor = gdk_cursor_new_for_display(gtk_widget_get_display(widget), GDK_HAND2);
-                    gdk_window_set_cursor(evt->window, cursor);
-                    g_object_unref(cursor);
-                }
-
-                xfdesktop_icon_view_invalidate_item(icon_view, icon_view->item_under_pointer, FALSE);
-            } else {
-                if (icon_view->single_click) {
-                    gdk_window_set_cursor(evt->window, NULL);
-                }
-            }
-        }
+        update_item_under_pointer(icon_view, evt->window, evt->x, evt->y);
     }
 
     gdk_event_request_motions(evt);
