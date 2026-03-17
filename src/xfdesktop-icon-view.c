xfce-mirror/xfdesktop

commit 8bf82aac67374b7bf7b66e0335bc2b3306fee83e

Fix leaks in xfdesktop_icon_view_constructed

src/xfdesktop-icon-view.c
@@ -1131,6 +1131,9 @@ xfdesktop_icon_view_finalize(GObject *obj)
     gtk_target_list_unref(icon_view->source_targets);
     gtk_target_list_unref(icon_view->dest_targets);
 
+    g_object_unref(icon_view->icon_renderer);
+    g_object_unref(icon_view->text_renderer);
+
     g_object_unref(icon_view->screen);
 
     G_OBJECT_CLASS(xfdesktop_icon_view_parent_class)->finalize(obj);

commit 8ef0fb69ed12a7d34c12008e41c2e6fb781c3bc3

Avoid unrefing objects that only have floating references
Previously, xfdesktop_icon_view_finalize() would trigger two
criticals like the following when running with
G_ENABLE_DIAGNOSTIC=1:

"GLib-GObject-CRITICAL **: A floating object GtkCellRendererPixbuf
... was finalized. This means that someone called g_object_unref()
on an object that had only a floating reference; the initial
floating reference is not owned by anyone and must be removed with
g_object_ref_sink()."

Fixes: 8bf82aac

src/xfdesktop-icon-view.c
@@ -1020,8 +1020,8 @@ xfdesktop_icon_view_init(XfdesktopIconView *icon_view)
 
     icon_view->draw_focus = TRUE;
 
-    icon_view->icon_renderer = gtk_cell_renderer_pixbuf_new();
-    icon_view->text_renderer = xfdesktop_cell_renderer_icon_label_new();
+    icon_view->icon_renderer = g_object_ref_sink(gtk_cell_renderer_pixbuf_new());
+    icon_view->text_renderer = g_object_ref_sink(xfdesktop_cell_renderer_icon_label_new());
 
     PangoAttrList *attr_list = NULL;
 #if PANGO_VERSION_CHECK (1, 44, 0)